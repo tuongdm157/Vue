86039a0bb503be39a48463e3f451a58fe80c3ae6
phan.anh <leducphananh@gmail.com>
2024-04-18 19:33:56 +0700

    add login screen

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,18 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
         path: "/",
         component: () => import("../views/Home/HelloWorld.vue"),
-    }, {
+    },
+    {
         path: "/counter",
         component: () => import("../views/Counter/Counter.vue"),
     },
-]
+    {
+        path: "/login",
+        component: () => import("../views/Login/Login.vue"),
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
-})
+});
 
-export default router
+export default router;
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-form @submit.prevent="onSubmit">
+    <v-img
+      class="mx-auto my-6"
+      max-width="228"
+      src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
+    ></v-img>
+
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      elevation="8"
+      max-width="448"
+      rounded="lg"
+    >
+      <div class="text-subtitle-1 text-medium-emphasis">Username</div>
+
+      <v-text-field
+        density="compact"
+        placeholder="Username"
+        prepend-inner-icon="mdi-email-outline"
+        variant="outlined"
+        v-model="user.username"
+      ></v-text-field>
+
+      <div
+        class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+      >
+        Password
+        <a
+          class="text-caption text-decoration-none text-blue"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Forgot login password?</a
+        >
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Enter your password"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+        v-model="user.password"
+      ></v-text-field>
+
+      <v-btn
+        block
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+        type="submit"
+      >
+        Log In
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <a
+          class="text-blue text-decoration-none"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+        </a>
+      </v-card-text>
+    </v-card>
+  </v-form>
+</template>
+
+<script lang="ts">
+import { ref, reactive } from "vue";
+
+export default {
+  setup() {
+    const visible = ref(false);
+    const user = reactive({
+      username: "",
+      password: "",
+    });
+
+    const onSubmit = () => {
+      console.log("data", user);
+    };
+
+    return {
+      visible,
+      user,
+      onSubmit,
+    };
+  },
+};
+</script>
