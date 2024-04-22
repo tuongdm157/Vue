7ab5d78320fb10ea8416e4144f9c1131ff25bb70
phan.anh <leducphananh@gmail.com>
2024-04-22 23:36:16 +0700

    sort table

--- src/views/Customer/CustomerList/CustomerList.vue
+++ src/views/Customer/CustomerList/CustomerList.vue
@@ -11,44 +11,72 @@
     </VCol>
   </VRow>
 
-  <VTable :hover="true">
-    <thead>
-      <tr>
-        <th>Email</th>
-        <th>Name</th>
-        <th>Checkbox</th>
-        <th>Gender</th>
-        <th>Country</th>
-        <th>Description</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="customer in customers"
-        :key="customer.id"
-        @click="() => router.push(`/customers/${customer.id}`)"
+  <VTextField
+    v-model="search"
+    label="Search"
+    prepend-inner-icon="mdi-magnify"
+    variant="outlined"
+    hide-details
+    single-line
+    density="compact"
+  ></VTextField>
+
+  <VDataTableServer
+    v-model:items-per-page="itemsPerPage"
+    :headers="headers"
+    :items="customers"
+    :items-length="totalItems"
+    @update:options="fetchCustomers"
+  >
+    <template v-slot:item.checkbox="{ item }">
+      <span v-for="cb in item.checkbox" :key="cb">
+        {{ cb }}
+        <br />
+      </span>
+    </template>
+
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        class="me-8"
+        size="small"
+        @click="() => router.push(`/customers/${item.id}`)"
       >
-        <td>{{ customer.email }}</td>
-        <td>{{ `${customer.firstName} ${customer.lastName}` }}</td>
-        <td>
-          <span v-for="item in customer.checkbox" :key="item">
-            {{ item }}
-            <br />
-          </span>
-        </td>
-        <td>{{ customer.gender }}</td>
-        <td>{{ customer.country }}</td>
-        <td>{{ customer.description }}</td>
-        <td>
-          <VIcon
-            icon="mdi-delete"
-            @click.stop="() => onDeleteCustomer(customer.id)"
-          />
-        </td>
-      </tr>
-    </tbody>
-  </VTable>
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        size="small"
+        @click="
+          () => {
+            dialogDelete.customerId = item.id;
+            dialogDelete.open = true;
+          }
+        "
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+  </VDataTableServer>
+
+  <v-dialog v-model="dialogDelete.open" width="fit-content">
+    <v-card>
+      <v-card-title> Are you sure you want to delete this item? </v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          variant="text"
+          @click="
+            () => {
+              dialogDelete.open = false;
+              dialogDelete.customerId = null;
+            }
+          "
+          >Cancel</v-btn
+        >
+        <v-btn variant="tonal" @click="onDeleteCustomer">OK</v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
@@ -60,21 +88,61 @@ import { Customer } from "../types";
 const router = useRouter();
 
 const customers = ref<(Customer & { id: string })[]>([]);
+const totalItems = ref(0);
+const itemsPerPage = ref(10);
 
-const fetchCustomers = async () => {
+const dialogDelete = ref({
+  open: false,
+  customerId: null,
+});
+
+const search = ref("");
+
+const fetchCustomers = async ({ page, itemsPerPage, sortBy }) => {
+  let sort = undefined;
+  if (sortBy.length) {
+    const [{ key, order }] = sortBy;
+    let orderBy = order === "asc" ? "" : "-";
+    sort = orderBy + key;
+  }
+
   try {
-    const response = await http.get("/customers");
+    const response = await http.get(`/customers/`, {
+      params: {
+        _page: page,
+        _per_page: itemsPerPage,
+        _sort: sort,
+      },
+    });
+    const totalItemsResponse = await http.get(`/customers/`, {
+      params: {
+        _sort: sort,
+      },
+    });
     if (response && response.status === 200) {
-      customers.value = response.data;
+      console.log("response", response);
+      customers.value = response.data.data;
+      totalItems.value = totalItemsResponse.data.length;
     }
   } catch (error) {
     console.error(error);
   }
 };
-fetchCustomers();
 
-const onDeleteCustomer = async (customerId: string) => {
-  await http.delete(`/customers/${customerId}`);
-  await fetchCustomers();
+const headers = [
+  { title: "Email", key: "email" },
+  { title: "First Name", key: "firstName" },
+  { title: "Last Name", key: "lastName" },
+  { title: "Checkbox", key: "checkbox", sortable: false },
+  { title: "Gender", key: "gender" },
+  { title: "Country", key: "country" },
+  { title: "Description", key: "description" },
+  { title: "Actions", key: "actions", sortable: false },
+];
+
+const onDeleteCustomer = async () => {
+  await http.delete(`/customers/${dialogDelete.value.customerId}`);
+  dialogDelete.value.customerId = null;
+  dialogDelete.value.open = false;
 };
 </script>
--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,72 +1,76 @@
 <template>
-  <VForm @submit.prevent="onSubmit">
-    <VCard
-      class="mx-auto pa-12 pb-8 pt-6"
-      elevation="8"
-      width="448"
-      rounded="lg"
-    >
-      <div class="text-center text-light-blue text-h6 mb-2">Welcome Back!</div>
-      <div class="mb-8 text-center text text-teal">Sign in to continue.</div>
-
-      <div class="text-body-2 text-grey-darken-4 mb-1">Username</div>
-
-      <VTextField
-        density="compact"
-        placeholder="Enter username"
-        prepend-inner-icon="mdi-email-outline"
-        variant="outlined"
-        v-model="user.username"
-      ></VTextField>
-
-      <div
-        class="text-body-2 text-grey-darken-4 d-flex align-center justify-space-between mb-1"
+  <div class="d-flex justify-center align-center h-screen bg-white">
+    <VForm @submit.prevent="onSubmit">
+      <VCard
+        class="mx-auto pa-12 pb-8 pt-6"
+        elevation="8"
+        width="448"
+        rounded="lg"
       >
-        Password
-        <a
-          class="text-caption text-decoration-none text-blue"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Forgot login password?</a
-        >
-      </div>
+        <div class="text-center text-light-blue text-h6 mb-2">
+          Welcome Back!
+        </div>
+        <div class="mb-8 text-center text text-teal">Sign in to continue.</div>
 
-      <VTextField
-        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="visible ? 'text' : 'password'"
-        density="compact"
-        placeholder="Enter password"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
-        @click:append-inner="visible = !visible"
-        v-model="user.password"
-      ></VTextField>
+        <div class="text-body-2 text-grey-darken-4 mb-1">Username</div>
 
-      <VBtn
-        block
-        class="mb-8"
-        color="blue"
-        size="large"
-        variant="tonal"
-        type="submit"
-      >
-        Log In
-      </VBtn>
+        <VTextField
+          density="compact"
+          placeholder="Enter username"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+          v-model="user.username"
+        ></VTextField>
 
-      <VCardText class="text-center">
-        <a
-          class="text-blue text-decoration-none"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
+        <div
+          class="text-body-2 text-grey-darken-4 d-flex align-center justify-space-between mb-1"
         >
-          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-        </a>
-      </VCardText>
-    </VCard>
-  </VForm>
+          Password
+          <a
+            class="text-caption text-decoration-none text-blue"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Forgot login password?</a
+          >
+        </div>
+
+        <VTextField
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Enter password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+          v-model="user.password"
+        ></VTextField>
+
+        <VBtn
+          block
+          class="mb-8"
+          color="blue"
+          size="large"
+          variant="tonal"
+          type="submit"
+        >
+          Log In
+        </VBtn>
+
+        <VCardText class="text-center">
+          <a
+            class="text-blue text-decoration-none"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </VCardText>
+      </VCard>
+    </VForm>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -81,7 +85,7 @@ const user = reactive({
 
 const onSubmit = () => {
   console.log("data", user);
-  localStorage.setItem('LoggedIn', 'loggedIn');
-  router.push('/home');
+  localStorage.setItem("LoggedIn", "loggedIn");
+  router.push("/home");
 };
 </script>
